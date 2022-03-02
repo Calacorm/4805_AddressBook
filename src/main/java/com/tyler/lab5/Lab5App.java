bbbc3438f069ac4c5447845f0b0f87f39a0c665e
Tyler Mair <calacorm@gmail.com>
2022-03-02 14:45:51 -0500

    test

--- src/main/java/com/tyler/lab5/Lab5App.java
+++ src/main/java/com/tyler/lab5/Lab5App.java
@@ -3,7 +3,6 @@ package com.tyler.lab5;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//Test Comment
 @SpringBootApplication
 public class Lab5App {
 
